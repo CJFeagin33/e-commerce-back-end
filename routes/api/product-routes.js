5b83330f3006cbae43cfaad9faa72f6de6176f7b
CJFeagin33 <135062039+CJFeagin33@users.noreply.github.com>
2023-09-19 17:33:19 -0700

    added routes for tags and products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,32 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  try{
+    const products = await Product.findAll({include: [Category, Tag]});
+    if(!products) {
+      return res.status(400).json({message: 'failed to find all products'});
+    };
+    res.status(200).json(products);
+  } catch (err) {res.status(500).json(err)};
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const product = await Product.findOne({
+      where: {id: req.params.id},
+      include: [Category, Tag],
+    })
+    if(!product) {
+      return res.status(400).json({message: 'failed to find product using that id'});
+    }
+    res.status(200).json(product);
+  } catch (err) {res.status(500).json(err)};
 });
 
 // create new product
@@ -92,8 +109,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const deleteProduct = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deleteProduct) {
+      return res.status(404).json({message: 'No Product found with this id'});
+    }
+    res.status(200).json(deleteProduct);
+  } catch (err) {res.status(500).json(err)}
 });
 
 module.exports = router;
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,26 +3,72 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tags = await Tag.findAll({include: [Product]});
+    if (!tags) {
+      return res.status(400).json({message: 'failed to find tags'});
+    };
+    res.status(200).json(tags);
+  } catch (err) {res.status(500).json(err)};
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tag = await Tag.findOne({
+      where: {id: req.params.id},
+      include: [Product],
+    })
+    if(!tag) {
+      return res.status(400).json({message: 'failed to find tag'});
+    }
+    res.status(200).json(tag);
+  } catch (err) {res.status(500).json(err)};
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const newTag = await Tag.create(req.body);
+    if(!newTag) {
+      return res.status(400).json({message: 'failed to create new tag'});
+    }
+    res.status(200).json(newTag);
+} catch (err) {res.status(500).json(err)};
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const updateTag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id,
+      }
+    })
+    if (!updateTag) {
+      return res.status(404).json({message: 'failed to update tag with this id'});
+    }
+    res.status(200).json(updateTag);
+  } catch (err) {res.status(500).json(err)};
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const deleteTag = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!deleteTag) {
+      return res.status(404).json({message: 'No tag found with this id'});
+    }
+    res.status(200).json(deleteTag);
+  } catch (err) {res.status(500).json(err)}
 });
 
 module.exports = router;
